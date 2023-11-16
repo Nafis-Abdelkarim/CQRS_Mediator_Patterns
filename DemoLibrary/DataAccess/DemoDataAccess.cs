b870e06129d9f10b01804bfc169bc2f09247c7f6
a.nefis <a.nefis@DSIDEVF11DEL6J2>
2023-11-16 11:50:46 +0100

    Add project files.

---
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,42 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonModel> people = new();
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel() { Id = 1, FirstName = "Joe", LastName = "Doe" });
+            people.Add(new PersonModel() { Id = 2, FirstName = "Sue", LastName = "Storm" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel GetPerson(int Id)
+        {
+            var person = people.Find(x => x.Id == Id);
+            
+            return person;
+        }
+
+        public PersonModel InsertPeople(string firstName, string lastName)
+        {
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+            p.Id = people.Max(x => x.Id) + 1;
+            people.Add(p);
+            return p;
+        }
+
+    }
+}
